9bc21c83d109f23e0392c9cf84e5a2f167018949
Tim Slippy, voxelv <slippytim@gmail.com>
2018-08-26 12:57:43 -0700

    some work on grid object idea

--- scripts/game_grid.gd
+++ scripts/game_grid.gd
@@ -11,3 +11,6 @@ func get_grid_rect():
 	var side_length = tilemap.cell_size.x * TILES_PER_SIDE
 	var rect = Rect2(position, Vector2(side_length, side_length))
 	return(rect)
+
+func request_move(grid_object, direction):
+	pass
---
+++ scripts/grid_object.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+enum GRID_OBJECT_TYPE {MULTI_CELL, BLOCK}
+export(GRID_OBJECT_TYPE) var type = BLOCK
+
